43493998c39ef38513dd59611ae4c07c7cee6c9c
go shibata <warahiko@gmail.com>
2020-04-13 06:50:06 +0900

    Fix: navigate up

--- app/src/main/java/com/example/go/githubcontributors/ui/MainActivity.kt
+++ app/src/main/java/com/example/go/githubcontributors/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.go.githubcontributors.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.go.githubcontributors.R
@@ -9,12 +10,16 @@ import dagger.android.AndroidInjection
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        navController = findNavController(R.id.nav_host_fragment)
         setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean = navController.navigateUp()
 }
